37e5e983561ec5e233eba26a528680dd2de18db5
Lucifer <Lucifer@LAPTOP-UV37TL61.dhcp.wustl.edu>
2023-10-06 14:06:25 -0500

    stdDraw

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,22 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
+	
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 20);
+		StdDraw.setYscale(0, 10);
+		StdDraw.clear();
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(4, 5, 3, 4);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(10, 5, 3, 4);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(16, 5, 3, 4);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5,5,0.5,5);
+		StdDraw.filledRectangle(19.5,5,0.5,5);
+		StdDraw.filledRectangle(10,0.5,9,0.5);
+		StdDraw.filledRectangle(10,9.5,9,0.5);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,73 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+			String name = in.next();
+			if (name.length()==7)
+			{
+				int R = in.nextInt();
+				int G = in.nextInt();
+				int B = in.nextInt();
+				boolean figure = in.nextBoolean();
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double a = in.nextDouble();
+				double b = in.nextDouble();
+				StdDraw.setPenColor(R,G,B);
+				if (figure == false)
+				{
+					StdDraw.ellipse(x,y,a,b);
+				}
+				else
+				{
+					StdDraw.filledEllipse(x,y,a,b);
+				}
+			}
+			else if (name.length()==9)
+			{
+				int R = in.nextInt();
+				int G = in.nextInt();
+				int B = in.nextInt();
+				boolean figure = in.nextBoolean();
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double a = in.nextDouble();
+				double b = in.nextDouble();
+				StdDraw.setPenColor(R,G,B);
+				if (figure == false)
+				{
+					StdDraw.rectangle(x,y,a,b);
+				}
+				else
+				{
+					StdDraw.filledRectangle(x,y,a,b);
+				}
+			}
+			else
+			{
+				int R = in.nextInt();
+				int G = in.nextInt();
+				int B = in.nextInt();
+				boolean figure = in.nextBoolean();
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y3 = in.nextDouble();
+				StdDraw.setPenColor(R,G,B);
+				if (figure == false)
+				{
+					StdDraw.line(x1,y1,x2,y2);
+					StdDraw.line(x1,y1,x3,y3);
+					StdDraw.line(x2,y2,x3,y3);
+				}
+				else
+				{
+					double[] x = {x1,x2,x3};
+					double[] y = {y1,y2,y3};
+					StdDraw.filledPolygon(x,y);
+				}
+			}
+		
 	}
 }
